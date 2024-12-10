4dd2c4b6295ad7df23bcc8a6a5103cde8024761b
Sanjay <sanjay.k@affinidi.com>
2024-12-10 14:01:14 +0530

    fix: css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { AppProps } from 'next/app'
 
 const inter = Inter({ subsets: ['latin'] })
-
-import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
